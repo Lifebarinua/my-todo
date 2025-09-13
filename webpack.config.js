be618fa7799b64d84849ebf31888a1e7120c56da
Lifebarinua <barinualife@gmail.com>
2025-09-13 18:52:16 +0100

    change docs back to dist

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     clean: true, // clears old dist before build
   },
   module: {
@@ -23,7 +23,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: "./docs",
+    static: "./dist",
     open: true,
     hot: true,
   },
